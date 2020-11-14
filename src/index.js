b070c90b35a6d227f0a2d886a39f7ac7fb51a287
ankynator <ankynator@gmail.com>
2020-11-13 21:29:31 -0500

    Entendiendo ReactDom.render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+// const element = document.createElement('h1')
+// element.innerText = "Render con js normalito"
 
-// const container = document.getElementById('app');
+// const container = document.getElementById('app')
+// container.appendChild(element)
 
-// container.appendChild(element);
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+const name = "walter"
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const element = <h1>Hello, Platzi from React!</h1>
+// const element = React.createElement('h1', {}, 'Hola soy los children')
 
-const container = document.getElementById('app');
+// const jsx = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a platzi')
+// const element = React.createElement('h1', {}, `Hola soy ${2+2}`)
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// const jsx = (
+//   <div>
+//     <h1>Hola soy Vander</h1>
+//     <p>Y soy estudiante</p>
+//   </div>
+// )
+
+const element = React.createElement(
+    'div', 
+    {},
+    React.createElement('h1', {}, 'Hola soy Vander'),
+    React.createElement('p', {}, 'Y soy un estudiantoso')
+  )
+
+
+const container = document.getElementById('app')
+
+ReactDOM.render(element, container)
+
