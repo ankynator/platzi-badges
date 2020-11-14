600ff5bb103e959ec9588af581720dde08d459a0
ankynator <ankynator@gmail.com>
2020-11-13 22:31:09 -0500

    Creando el primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src="" alt="logo platziConf"/>
+        </div>
+
+        <div>
+          <img src="" alt="Avatar"/>
+          <h1>Vander <br/> Idme</h1>
+        </div>
+        
+        <div>
+          <p>Student</p>
+          <p>@ankynator</p>
+        </div>
+
+        <div>
+          #platziconf
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Badge
--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-// const element = document.createElement('h1')
-// element.innerText = "Render con js normalito"
-
-// const container = document.getElementById('app')
-// container.appendChild(element)
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const name = "walter"
-
-// const element = <h1>Hello, Platzi from React!</h1>
-// const element = React.createElement('h1', {}, 'Hola soy los children')
-
-// const jsx = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a platzi')
-// const element = React.createElement('h1', {}, `Hola soy ${2+2}`)
-
-// const jsx = (
-//   <div>
-//     <h1>Hola soy Vander</h1>
-//     <p>Y soy estudiante</p>
-//   </div>
-// )
-
-const element = React.createElement(
-    'div', 
-    {},
-    React.createElement('h1', {}, 'Hola soy Vander'),
-    React.createElement('p', {}, 'Y soy un estudiantoso')
-  )
-
+import Badge from './components/Badge'
 
 const container = document.getElementById('app')
 
-ReactDOM.render(element, container)
+ReactDOM.render(<Badge />, container)
 
