e86cc8226b670ce99830ba2260c29d95c6e87aa4
ankynator <ankynator@gmail.com>
2020-11-14 21:32:24 -0500

    Aplicando estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,27 @@
 import React from 'react'
 
+import "./styles/Badge.css"
+import confLogo from '../images/badge-header.svg'
+
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <img src="" alt="logo platziConf"/>
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="logo de la platziConf"/>
         </div>
 
-        <div>
-          <img src="" alt="Avatar"/>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
           <h1>Vander <br/> Idme</h1>
         </div>
         
-        <div>
-          <p>Student</p>
-          <p>@ankynator</p>
+        <div className="Badge__section-info">
+          <h3>Student</h3>
+          <div>@ankynator</div>
         </div>
 
-        <div>
+        <div className="Badge__footer">
           #platziconf
         </div>
       </div>
