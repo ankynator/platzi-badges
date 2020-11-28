d8c621e9dbada21c504b6c0d146483592262f7be
ankynator <ankynator@gmail.com>
2020-11-27 22:56:15 -0500

    Mejorando la experiencia de usuario durante una peticion

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './styles/Loader.css'
+
+function Loader() {
+  return(
+    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default Loader 
