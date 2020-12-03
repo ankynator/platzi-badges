4fb859e49e97261cbd5353b484dbf640d78fc8d3
ankynator <ankynator@gmail.com>
2020-12-02 19:09:56 -0500

    UI components y container components

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,51 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import PageLoading from '../components/PageLoading'
-import PageError from '../components/PageError'
 import Badge from '../components/Badge'
+
+import confLogo from '../images/platziconf-logo.svg'
 
 import './styles/BadgeDetails.css'
 
-import confLogo from '../images/platziconf-logo.svg'
-import api from '../api'
-
-class BadgeDetails extends React.Component {
-
-  state = {
-    loading: true,
-    error: null,
-    data: undefined
-  }
-
-  componentDidMount() {
-    this.fetchData()
-  }
-
-  fetchData = async (e) => {
-    this.setState({ loading:true, error: null })
-
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId)
-
-      this.setState({ loading:false, data: data })
-    } catch (error) {
-      this.setState({ loading: false, error: error})
-    }
-  }
-
-  render() {
-    if (this.state.loading) {
-      return <PageLoading />
-    }
-    if (this.state.error) {
-      return <PageError error={this.state.error}/>
-    }
-
-    const badge = this.state.data
-
-    return (
-      <div>
+function BadgeDetails (props) {
+  const badge = props.badge
+  return (
+    <div>
         <div className="BadgeDetails__hero">
         <div className="container">
           <div className="row">
@@ -90,8 +55,7 @@ class BadgeDetails extends React.Component {
         </div>
       
       </div>
-    )  
-  }
+  )
 }
 
 export default BadgeDetails
